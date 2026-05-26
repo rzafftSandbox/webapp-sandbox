30e8d52dc528340d6f40095d67c36d3badbfc3f8
rzafftSandbox <rzafft18@gmail.com>
2026-05-25 18:33:47 -0600

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,8 @@ function App() {
     <div className='App'>
       <h1>Hello Sandbox</h1>
       <h2>Lets start creating!</h2>
-      <MapContainer center={[42.583, 107.403]} zoom={25}>
+      <MapContainer center={[43.0002, -107.554565]} zoom={8}>
         <TileLayer
-          attribution='https://www.openstreetmap.org/copyright'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
       </MapContainer>
